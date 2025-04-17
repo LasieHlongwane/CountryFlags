02bc947ce951516af1242751aed0f9a42d581f50
Lasie Hlongwane <lasielazba1@gmail.com>
2025-04-17 11:41:28 +0200

    Fix: Add STATIC_ROOT for static files collection

--- main/main/settings.py
+++ main/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'countries',
     'rest_framework',
     'corsheaders',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -118,11 +120,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+# settings.py
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
