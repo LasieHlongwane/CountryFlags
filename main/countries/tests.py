0b2e5b053babeea68f72fce0da3d532e6e912770
Lasie Hlongwane <lasielazba1@gmail.com>
2025-04-17 13:11:32 +0200

    Fix nested repository issue

--- main/countries/tests.py
+++ main/countries/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Country  # only if you store countries locally
 
-# Create your tests here.
+class CountryAPITests(APITestCase):
+
+    def test_country_list_endpoint(self):
+        """
+        Ensure the /api/countries/ endpoint returns a 200 response.
+        """
+        url = reverse('country-list')  # make sure your URL name matches
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_country_list_response_format(self):
+        """
+        Test that response contains a list of countries with expected fields.
+        """
+        url = reverse('country-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.data, list)
+
+        if response.data:
+            country = response.data[0]
+            self.assertIn('name'),
+
--- main/countries/urls.py
+++ main/countries/urls.py
@@ -4,6 +4,9 @@ from .views import CountryListView, CountryDetailView
 urlpatterns = [
     path('countries/', CountryListView.as_view(), name='country-list'),
     path('countries/<str:name>/', CountryDetailView.as_view(), name='country-detail'),
+    path('countries/', CountryListView.as_view(), name='country-list'),
+
 ]
+
 
 
