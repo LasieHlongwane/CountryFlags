4c617f525ac04ec4326c28fff7a1c61d9f95cec9
Lasie Hlongwane <lasielazba1@gmail.com>
2025-04-17 13:23:27 +0200

    Fix backend tests

--- main/countries/tests.py
+++ main/countries/tests.py
@@ -1,29 +1,23 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from .models import Country  # only if you store countries locally
 
-class CountryAPITests(APITestCase):
-
-    def test_country_list_endpoint(self):
-        """
-        Ensure the /api/countries/ endpoint returns a 200 response.
-        """
-        url = reverse('country-list')  # make sure your URL name matches
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+class CountryAPITests(TestCase):
+    
     def test_country_list_response_format(self):
-        """
-        Test that response contains a list of countries with expected fields.
-        """
-        url = reverse('country-list')
-        response = self.client.get(url)
+        # Make a GET request to the country list endpoint
+        response = self.client.get(reverse('country-list'))
+        
+        # Assert that the response status is 200 OK
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.data, list)
-
+        
+        # If the response contains data, check for the expected keys
         if response.data:
-            country = response.data[0]
-            self.assertIn('name'),
+            country = response.data[0]  # Get the first country in the response
+            
+            # Check if 'name' and 'flag' exist in the country dictionary
+            self.assertIn('name', country)
+            self.assertIn('flag', country)
+            # You can add additional checks for other fields here if needed
+
+
 
